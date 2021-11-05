26c7756bc47b61d67dd2c5ada15163d3ae506e76
GrimReaper2654 <80506189+GrimReaper2654@users.noreply.github.com>
2021-11-05 18:51:07 +0800

    bot.js now contains code for the user
    MACE SPEDLORD MACE

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 //
-// ----------Innovations simulation----------
-// [placeholder]
-// [placeholder]
-//-------------------------------------------
+// ---------------Innovations simulation---------------
+// bot(maxday, botnum) to run a simulation.
+// when botnum == 69 bot will always make best choice.
+// Mace edit the game function on line 89.
+//-----------------------------------------------------
 // by Tom
 //
 
 const wheatConstant = 10;
-// maxday should be 15, 8, 28, 31
+
 
 function Code(price, wheat, money, day, maxday) {
     maxday=none;
@@ -20,20 +21,17 @@ function Code(price, wheat, money, day, maxday) {
 function simpleBot(price, wheat, money, day, maxday, botnum) {
     var choice = 'farm' // return 'farm' or 'sell'
     // Insert code here...
+    if (botnum == 69) {
+        if (day==maxday) {
+            choice = 'sell';
+        } else {
+            choice = 'farm';
+        }
+    return choice;
+    }
     if (day%botnum==0&&wheat>0) {
         choice = 'sell';
     }else {
-        choice = 'farm';
-    }
-    return choice;
-}
-
-function spedBot(price, wheat, money, day, maxday) {
-    var choice = 'farm' // return 'farm' or 'sell'
-    // Insert code here...
-    if (day==maxday) {
-        choice = 'sell';
-    } else {
         choice = 'farm';
     }
     return choice;
@@ -88,22 +86,57 @@ function bot(maxday, botnum=1) {
     return money;
 }
 
-var bestBot=-1;
-var bestBotScore = -1;
-for (var i = 2; i <= 20; i++) {
-    var totalScore=0;
-    for (var j = 1; j <= 5; j++) {
-        totalScore += bot(j, i);
+function game(maxday) {
+    var wheat = 10;
+    var money = 0;
+    var price = 1;
+    var income = 0;
+    for (var day = 0; day < maxday; day++) {
+        console.log(`DEBUG:   Day ${day+1}/${maxday} starts with ${wheat} wheat and ${Math.round(money)} money.`);
+        var userInput =  // User input goes here
+        price = IncreasePrice(price);
+        if (userInput == 'farm') {
+            wheat+=10;
+            console.log(`INFO:    User chose to farm increasing wheat by ${wheatConstant}.`);
+        } else if (userInput == 'sell') {
+            if (wheat==0) {
+                console.warn(`WARNING: User tried to sell 0 wheat.`);
+            }
+            income=wheat*price;
+            money+=income;
+            console.log(`INFO:    User sold ${wheat} for $${Math.round(price * 10) / 10}/unit earning $${Math.round(income * 10) / 10}.`);
+            wheat=0;
+        }else {
+            console.error(`ERROR:   User made choice '${userInput}' which is not 'farm' or 'sell'.`);
+            console.warn(`WARNING: Day skipped since user made incorrect choice.`);
+        }
+        console.log(`DEBUG:   Day ${day+1}/${maxday} ends with ${wheat} wheat and $${Math.round(money * 10) / 10} money.`);
     }
-    console.log(`INFO:    Total score for bot: $${Math.round(totalScore * 10) / 10}.`);
-    if (totalScore > bestBotScore) {
-        bestBot = i;
-        bestBotScore=totalScore;
-    }
+    console.log(`INFO:    Game ended on day ${maxday} with user scoring $${Math.round(money * 10) / 10} and having ${wheat} wheat unsold.`);
+    return money;
 }
-console.log(`INFO:    Best bot step size: ${bestBot} with score ${bestBotScore}.`);
+
+// Find best bot for max number of rounds
+function optimiseBot(maxrounds) {
+    var bestBot=-1;
+    var bestBotScore = -1;
+    for (var i = 2; i <= maxrounds/2; i++) {
+        var totalScore=0;
+        for (var j = 1; j <= maxrounds; j++) {
+            totalScore += bot(j, i);
+        }
+        console.log(`INFO:    Total score for bot: $${Math.round(totalScore * 10) / 10}.`);
+        if (totalScore > bestBotScore) {
+            bestBot = i;
+            bestBotScore=totalScore;
+        }
+    }
+    console.log(`INFO:    Best bot step size: ${bestBot} with score ${bestBotScore}.`);
+    return bestBot;
+}
 
 // Best bot with 50 day limit: 11 with $30040
 // Best bot with 30 day limit: 9 with $7858
 // Best bot with 15 day limit: 5 with $1405
 // Best bot with 5 day limit: 2 with $124
+
